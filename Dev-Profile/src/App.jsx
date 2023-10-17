6f5153849b0eed470542f818c04b27ec83d136bb
skm1803 <shubham.mishrakiit@gmail.com>
2023-10-17 11:29:52 +0530

    Destructuring props updated on skill list using rendring list and condition rendering with &&

--- Dev-Profile/src/App.jsx
+++ Dev-Profile/src/App.jsx
@@ -1,12 +1,104 @@
-import './App.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA"
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D"
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF"
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33"
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB"
+  },
+  {
+    skill: "COBOL",
+    level: "intermediate",
+    color: "#FF3B00"
+  },
+  {
+    skill: "JAVA",
+    level: "intermediate",
+    color: "#FF3B00"
+  }
+];
 
 function App() {
-
   return (
-    <>
-    <h1>Dev profile Card</h1>
-    </>
-  )
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
 }
 
-export default App
+function Avatar() {
+  return <img className="avatar" src="spiderman.jpg" alt="Jonas Schmedtmann" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Shubham Kumar Mishra</h1>
+      <p>
+        Frontend web developer at TCS. Coding for customization and devlopement of CBS. When not coding, I like to read Books.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill, color, level }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+export default App;
