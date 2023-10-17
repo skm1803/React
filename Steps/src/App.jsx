bc5c546d1273a0308e730a525381620b6477631a
skm1803 <shubham.mishrakiit@gmail.com>
2023-10-17 16:30:33 +0530

    used state variable to update steps count

--- Steps/src/App.jsx
+++ Steps/src/App.jsx
@@ -6,9 +6,18 @@ const messages = [
   'Apply for jobs 💼',
   'Invest your new income 🤑',
 ];
-
 function App() {
-  const step=0;
+
+  const [step, setStep]=useState(1);
+
+
+  function handelNext()
+  {
+   if (step<3) setStep(step+1);
+  }
+  function handelPrevious(){
+   if (step>1) setStep(step-1);
+  }
   return (
     <div className='steps'>
       <div className='numbers'>
@@ -18,8 +27,8 @@ function App() {
       </div>
       <p className='message'>Step{step}:{messages[step-1]}</p>
       <div className='buttons'>
-        <button style={{backgroundColor:'#7950f2', color:'#fff'}}>Previous</button>
-        <button style={{backgroundColor:'#7950f2', color:'#fff'}}>Next</button>
+        <button style={{backgroundColor:'#7950f2', color:'#fff'}} onClick={handelPrevious}>Previous</button>
+        <button style={{backgroundColor:'#7950f2', color:'#fff'}} onClick={handelNext }>Next</button>
 
       </div>
       
